b8651bbde7baad1950b58f74a566776ac36cddf5
Robert <zhaorobert5@gmail.com>
2026-03-02 19:04:19 -0800

    75%

--- src/lab5.c
+++ src/lab5.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <EFM8LB1.h>
+#include <string.h>
 #include "uart.h"
 #include "cmd.h"
 
 #define SYSCLK 72000000L
 #define BAUDRATE 115200L
 #define SARCLK 18000000L
+
+#define LCD_RS P1_7
+// #define LCD_RW Px_x // Not used in this code. Connect to GND
+#define LCD_E P2_0
+#define LCD_D4 P1_3
+#define LCD_D5 P1_2
+#define LCD_D6 P1_1
+#define LCD_D7 P1_0
+#define CHARS_PER_LINE 16
 
 unsigned int last_accept_frequency = 0;
 
@@ -326,6 +336,78 @@ unsigned int measure_zero_cross_time(void){
     return time_us;
 }
 
+/*==================================================
+ These are the LCD helper functions. Feel free
+ to move them into other files when organizing :)
+===================================================*/
+void LCD_pulse (void)
+{
+	LCD_E=1;
+	Timer3us(40);
+	LCD_E=0;
+}
+
+void LCD_byte (unsigned char x)
+{
+	// The accumulator in the C8051Fxxx is bit addressable!
+	ACC=x; //Send high nible
+	LCD_D7=ACC_7;
+	LCD_D6=ACC_6;
+	LCD_D5=ACC_5;
+	LCD_D4=ACC_4;
+	LCD_pulse();
+	Timer3us(40);
+	ACC=x; //Send low nible
+	LCD_D7=ACC_3;
+	LCD_D6=ACC_2;
+	LCD_D5=ACC_1;
+	LCD_D4=ACC_0;
+	LCD_pulse();
+}
+
+void WriteData (unsigned char x)
+{
+	LCD_RS=1;
+	LCD_byte(x);
+	waitms(2);
+}
+
+void WriteCommand (unsigned char x)
+{
+	LCD_RS=0;
+	LCD_byte(x);
+	waitms(5);
+}
+
+void LCD_4BIT (void)
+{
+	LCD_E=0; // Resting state of LCD's enable is zero
+	// LCD_RW=0; // We are only writing to the LCD in this program
+	waitms(20);
+	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+	WriteCommand(0x33);
+	WriteCommand(0x33);
+	WriteCommand(0x32); // Change to 4-bit mode
+
+	// Configure the LCD
+	WriteCommand(0x28);
+	WriteCommand(0x0c);
+	WriteCommand(0x01); // Clear screen command (takes some time)
+	waitms(20); // Wait for clear screen command to finsih.
+}
+
+void LCDprint(char * string, unsigned char line, bit clear)
+{
+	int j;
+
+	WriteCommand(line==2?0xc0:0x80);
+	waitms(5);
+	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
+	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+}
+
+
+
 void main(void){
     // Measured voltage variables
     float v1;
@@ -334,6 +416,7 @@ void main(void){
     unsigned int frequency;
 	unsigned int zero_time_diff;
 	float phase_angle;
+	char buff[16]; // LCD buffer
 
     waitms(500);
     printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
@@ -348,7 +431,10 @@ void main(void){
     InitADC();
     TIMER0_Init();
     init_pin_input();
-
+	
+	// Configure LCD
+	LCD_4BIT();
+	
     while(1){
         v1 = read_ripple_voltage(QFP32_MUX_P2_1); // v1 is the reference voltage
         v2 = read_ripple_voltage(QFP32_MUX_P1_6);
@@ -408,6 +494,15 @@ void main(void){
 				ref_which_signal = 0;
 			}
 		}
+
+		// Display format:
+		// Vr = x.xv xHz
+		// Vn = x.xv x (degree)
+		sprintf(buff, "Vr=%.2fv %uHz", v1, frequency);
+		LCDprint(buff, 1, 1);
+		sprintf(buff, "Vm=%.2fv %.1f%c", v2, phase_angle, 0xDF);
+		LCDprint(buff, 2, 1);
+
         waitms(500);
     }
     
